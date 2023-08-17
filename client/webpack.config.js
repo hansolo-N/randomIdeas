a2e3546786d2c871ea2cbe45e4f100165154cdc1
hansolo-N <91372832+hansolo-N@users.noreply.github.com>
2023-08-17 09:21:12 +0200

    modified webpack for minify css

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,16 +3,18 @@ const path = require('path')
 
 const HtmlWebpackPlugin = require('html-webpack-plugin') 
 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
 module.exports = {
     mode: "development",
     entry: "./src/index.js",
     output:{
-        path: path.resolve(__dirname,'dist'),
+        path: path.resolve(__dirname,'public'),
         filename: "bundle.js"
     },
     devServer:{
         static:{
-            directory:path.resolve(__dirname,'dist')
+            directory:path.resolve(__dirname,'public')
         },
         port: 3000,
         open:true,
@@ -24,7 +26,7 @@ module.exports = {
         rules:[
             {
             test:/\.css$/,
-            use: ['style-loader','css-loader']
+            use: [MiniCssExtractPlugin.loader,'css-loader']
         }
     ]
     },
@@ -32,7 +34,8 @@ module.exports = {
         new HtmlWebpackPlugin({
             title: "Webpack App",
             filename: "index.html"
-        })
+        }),
+        new MiniCssExtractPlugin()
     ]
 
 };
